90edcb53dad1507da726d694d3fc9d9a991ce558
dyep49 <dyep49@gmail.com>
2014-06-17 20:50:32 -0400

    changed file path again, maybe it'll actually work?

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var app = angular.module("ATUS", ['ngResource', 'ngRoute', 'd3']);
 app.config(function($routeProvider){
   $routeProvider
     .when('/',{
-      templateUrl : '/templates/home.html',
+      templateUrl : './../templates/home.html',
       controller  : 'HomeCtrl'
     })
 });
