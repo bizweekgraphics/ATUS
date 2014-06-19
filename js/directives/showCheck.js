05ded586e36ab62da3e08c5c425aa01002620e9f
dyep49 <dyep49@gmail.com>
2014-06-19 18:07:26 -0400

    changed radios to checkboxes

--- js/directives/showCheck.js
+++ js/directives/showCheck.js
@@ -1,15 +1,52 @@
 app.directive('showCheck', function() {
   return {
     link:function(scope, element) {
-      $(element).on('change', {scope: scope, element: element}, function(event) {
-          $('.checkbox').attr('disabled', false)
+      $(element).on('mousedown', function(event) {
+        var element = event.target
+        var elClass = $(element).attr('class').split(' ')[0]
+        $('.' + elClass).attr('checked', false)
+      })
+      $(element).on('change', function(event) {
           var element = event.target
-          var elClass = $(element).attr('class')
-          $('.' + elClass).each(function(index, el) {
-            if(!el.checked) {
-              $(el).attr('disabled', true)
-            }
-          })
+          var elClass = $(element).attr('class').split(' ')[0]
+          switch(elClass) {
+            case "average":
+              $('.day').attr('disabled', true)
+              $('.education').attr('disabled', true)
+              $('.race').attr('disabled', true)
+              $('.age').attr('disabled', true)
+              $('.gender').attr('disabled', true)
+              break;
+            case "gender":
+              $('.day').attr('disabled', true)
+              $('.education').attr('disabled', true)
+              $('.average').attr('disabled', true)
+              break;
+            case "race":
+              $('.day').attr('disabled', true)
+              $('.education').attr('disabled', true)
+              $('.age').attr('disabled', true)
+              $('.average').attr('disabled', true)
+              break;
+            case "education":
+              $('.day').attr('disabled', true)
+              $('.race').attr('disabled', true)
+              $('.gender').attr('disabled', true)
+              $('.average').attr('disabled', true)
+              break;
+            case "age":
+              $('.day').attr('disabled', true)
+              $('.education').attr('disabled', true)
+              $('.race').attr('disabled', true)
+              $('.average').attr('disabled', true)
+              break;
+          }
+
+          // $('.' + elClass).each(function(index, el) {
+          //   if(!el.checked) {
+          //     $(el).attr('disabled', true)
+          //   }
+          // })
       })
     }
   }
