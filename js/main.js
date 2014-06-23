c4cdeb19b71b8b4ddfda17cd03392614656242e8
dyep49 <dyep49@gmail.com>
2014-06-23 19:16:33 -0400

    fixed relative path i think

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var app = angular.module("ATUS", ['ngResource', 'ngRoute', 'd3', 'ui.bootstrap',
 app.config(function($routeProvider){
   $routeProvider
     .when('/',{
-      templateUrl : '../templates/home.html',
+      templateUrl : 'templates/home.html',
       controller  : 'HomeCtrl'
     })
 });
