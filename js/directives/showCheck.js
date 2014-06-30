56bcdf21ec6ee55cc9f209a028e3549dc951b116
dyep49 <dyep49@gmail.com>
2014-06-30 15:39:41 -0400

    selects enable and disable based on data

--- js/directives/showCheck.js
+++ js/directives/showCheck.js
@@ -1,35 +1,39 @@
 app.directive('showCheck', function() {
   return {
     link:function(scope, element) {
-      $(element).on('click', function(event) {
+      $(element).on('change', function(event) {
 
           var disableEl = function(array) {
             array.forEach(function(item) {
-              $(item).attr('disabled', true)
+              $(item).prop('disabled', true)
             })
           }
 
-          var element = event.target
-          var elementId = $(element).attr('id')
+          var element = $(event.target)
+          var elementVal = element.val()
+          var elementId = element.find('option').filter(function(index, item) {
+            return $(item).val() === elementVal
+          })
+          var group = element.attr('class')
           var checkedArray = []
-          $('.radio-input').each(function(index, item) {
-            var radioEl = $(item)
-            var allEl = /All/.test(radioEl.attr('id'))
-            if(!allEl && item.checked === true) {
-              var elClass = $(item).attr('class').split(' ')[0]
+          $('option').each(function(index, item) {
+            var optionEl = $(item)
+            var allEl = /All/.test(optionEl.text())
+            if(!allEl && item.selected === true) {
+              var elClass = $(item).parent().attr('class').split(' ')[0]
               checkedArray.push(elClass)
             }
           })
 
-          var elementChecked = /All/.test(elementId) ? false : element.checked
+          var elementChecked = /All/.test(elementId) ? false : element.selected
 
-          scope.$parent.$parent.updateGraph()
-          $('.radio-input').attr('disabled', false)
+          scope.$parent.updateGraph()
+          $('select').attr('disabled', false)
           if(checkedArray.length > 0) {
             checkedArray.forEach(function(elClass) {
               switch(elClass) {
                 case "gender":
-                  disableEl(['.day', '.education', '.average'])
+                  disableEl(['.day', '.education'])
                   break;
                 case "day":
                   disableEl(['.education', '.race', '.age', '.gender'])
@@ -44,16 +48,15 @@ app.directive('showCheck', function() {
                   disableEl(['.day', '.education', '.race'])
                   break;
                default:
-                  $('.radio-input').attr('disabled', false)
+                  $('select').attr('disabled', false)
                   break;
               } 
             })
           } else {
-            $('.radio-input').attr('disabled', false)  
+            $('select').attr('disabled', false)  
           }  
-        $('.radio-input').each(function(index, item) {
-          var el = $(item).parent()
-          item.disabled === true ? el.css('opacity', .4) : el.css('opacity', 1)
+        $('select').each(function(index, item) {
+          item.disabled === true ? $(item).css('opacity', .4) : $(item).css('opacity', 1)
         })
       })
     }
